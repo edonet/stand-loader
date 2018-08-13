71371dec3971a0d240eaa77b1a808ea0912c666a
edonet <edonet@163.com>
2018-08-13 14:37:00 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # stand-loader
 stand in modules for webpack
+
+
+## Installation
+npm
+``` shell
+$ npm install -g @arted/stand-loader
+```
+
+or yarn
+``` shell
+$ yarn global add @arted/stand-loader
+```
+
+## Usage
+``` javascript
+// webpack.config.js
+const standloader = require('@arted/stand-loader');
+
+module.exports = {
+	...
+    module: {
+        rules: [
+            ...
+            standLoader({
+                test: '/path/to/module.js'
+                ...
+            })
+        ]
+    }
+};
+
+// app.js
+import mod from '@arted/stand-loader?name=test';
+```
---
+++ loader.js
@@ -0,0 +1,52 @@
+/**
+ *****************************************
+ * Created by lifx
+ * Created on 2018-08-13 13:52:24
+ *****************************************
+ */
+'use strict';
+
+
+/**
+ *****************************************
+ * 加载依赖
+ *****************************************
+ */
+const utils = require('loader-utils');
+
+
+/**
+ *****************************************
+ * 定义模块
+ *****************************************
+ */
+module.exports = function loader() {};
+
+
+/**
+ *****************************************
+ * 定义加载内容
+ *****************************************
+ */
+module.exports.pitch = function pitch() {
+    let query = this.resourceQuery && utils.parseQuery(this.resourceQuery);
+
+    // 处理模块
+    if (query && query.name) {
+        let options = utils.getOptions(this),
+            local = options && options[query.name];
+
+        // 处理回调
+        if (typeof local === 'function') {
+            local = local.call(this, query);
+        }
+
+        // 替换模块
+        if (local && typeof local === 'string') {
+            return `module.exports = require('${ local }'${ this.resourceQuery });`;
+        }
+    }
+
+    // 返回空
+    return '';
+};
